57739f9e910e13d31e156214a7cd03a7043babff
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-05-03 09:09:28 +0530

    update form details

--- assets/forms/contact.php
+++ assets/forms/contact.php
@@ -1,34 +1,34 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect input from the form
+    $name = strip_tags(trim($_POST["w3lName"]));
+    $email = filter_var(trim($_POST["w3lSender"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["w3lSubject"]));
+    $website = strip_tags(trim($_POST["w3lWebsite"]));
+    $message = trim($_POST["w3lMessage"]);
 
-  $receiving_email_address = 'dilshankavinda371@gmail.com';
+    // Specify your email and subject
+    $to = 'your_email@example.com';  // <-- Your email address here
+    $email_subject = "New contact from $name: $subject";
+    $email_body = "You have received a new message from the user $name.\n".
+                  "Here are the details:\n".
+                  "Name: $name\n".
+                  "Email: $email\n".
+                  "Website: $website\n".
+                  "Message: $message";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    // Headers
+    $headers = "From: $email\n";
+    $headers .= "Reply-To: $email";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    // Send the email
+    mail($to, $email_subject, $email_body, $headers);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    // Redirect to a new page or display a success message
+    echo "Thank you for your message. We will get back to you soon.";
+} else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
 ?>
